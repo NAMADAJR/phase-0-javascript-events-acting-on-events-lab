4ba7466636f5d36875423880c25562e7f5eb479d
Namada Junior <namada871@gmail.com>
2024-07-09 18:27:09 +0300

    Complete lab

--- index.js
+++ index.js
@@ -1,17 +1,33 @@
 // Your code here
 
+const dodger = document.getElementById("dodger");
+
 function moveDodgerLeft() {
-    var leftNumbers = dodger.style.left.replace('px', '');
-    var left = parseInt(leftNumbers, 10)
-    if (left > 0) {
-      dodger.style.left = `${left - 1}px`
-    }
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
   }
-  
-  function moveDodgerRight() {
-    var leftNumbers = dodger.style.left.replace('px', '');
-    var left = parseInt(leftNumbers, 10)
-    if (left < 360) {
-      dodger.style.left = `${left + 1}px`
-    }
+}
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "ArrowLeft") {
+    moveDodgerLeft();
   }
+});
+
+function moveDodgerRight() {
+  const rightNumbers = dodger.style.left.replace("px", "");
+  const right = parseInt(rightNumbers, 10);
+
+  if (right < window.innerWidth - dodger.offsetWidth) {
+    dodger.style.left = `${right + 1}px`;
+  }
+}
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "ArrowRight") {
+    moveDodgerRight();
+  }
+});
